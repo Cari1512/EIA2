fe9fe153d6264f21635d87e9d0359a245d533b24
Cari1512 <56436935+Cari1512@users.noreply.github.com>
2020-05-09 22:27:30 +0200

    Teil 1

--- L03/Aufgabe_3/Haushaltshilfe.js
+++ L03/Aufgabe_3/Haushaltshilfe.js
@@ -41,36 +41,134 @@ var Haushaltshilfe;
         let column = document.querySelector("form#column1");
         column.appendChild(Box);
     }
+    //Probleme: Bei ändern der Erledigung müssen die alten forms gelöscht werden
+    //Bei Neuer Erledigung wird bei jedem Case nur noch das Zuletzt genutzte case dargestellt
     function Add(_event) {
         let selectedTask = document.querySelector(".Erledigung").value;
         console.log(selectedTask);
         //let currentform:HTMLElement =<HTMLElement>document.querySelector();
         let deleter = document.createElement("i");
         deleter.setAttribute("class", "far fa-trash-alt");
+        deleter.setAttribute("id", "trashform");
         let form = document.createElement("div");
         form.setAttribute("class", "form");
         form.addEventListener("change", displayOrder);
-        let Box = document.querySelector("div.box");
+        let target = _event.target;
+        let Box = target.parentNode;
         Box.appendChild(form);
         form.appendChild(deleter);
         switch (selectedTask) {
             case "Einkaufen":
-                Einkaufen; //Warum kann die Funktion nicht ausgeführt werden
+                Einkaufen; //Warum kann die Funktion nicht ausgeführt werden?
+                let what1 = document.createElement("input");
+                what1.setAttribute("class", "Artikel");
+                what1.placeholder = "Was möchten Sie Einkaufen?";
+                let amount1 = document.createElement("input");
+                amount1.setAttribute("class", "Menge");
+                amount1.placeholder = "1";
+                let label1 = document.createElement("label");
+                label1.innerText = "Menge";
+                label1.htmlFor = "Menge";
+                let unit1 = document.createElement("select");
+                unit1.setAttribute("class", "Einheit");
+                var array = ["Pack", "Gramm", "kg", "Liter", "ml", "Flaschen", "Leib", "Karton"];
+                for (var i = 0; i < array.length; i++) {
+                    var option = document.createElement("option");
+                    option.value = array[i];
+                    option.text = array[i];
+                    unit1.appendChild(option);
+                }
+                ;
+                var array1 = ["Aldi", "Lidl", "Netto", "Rewe"];
+                let radiospan = document.createElement("span");
+                for (var i = 0; i < array1.length; i++) {
+                    let where = document.createElement("input");
+                    where.setAttribute("type", "radio");
+                    where.value = array1[i];
+                    where.id = array1[i];
+                    where.name = "Container";
+                    let label = document.createElement("label");
+                    label.htmlFor = array1[i];
+                    label.innerText = array1[i];
+                    radiospan.appendChild(label);
+                    radiospan.appendChild(where);
+                }
+                ;
+                form.appendChild(what1);
+                form.appendChild(label1);
+                form.appendChild(amount1);
+                form.appendChild(unit1);
+                form.appendChild(radiospan);
                 break;
             case "Haushalt":
-                Haushalt; //Warum kann die Funktion nicht ausgeführt werden
+                Haushalt; //Warum kann die Funktion nicht ausgeführt werden?
                 console.log("Haushaltfunction");
                 let what = document.createElement("input");
-                what.setAttribute("class", "Artikel");
+                what.setAttribute("class", "Aufgabe");
                 what.placeholder = "Wie können wir Sie unterstützen?";
+                what.setAttribute("list", "Aufgaben"); //wie Aufgaben Datalist erstellen
                 let amount = document.createElement("input");
                 amount.setAttribute("class", "Dauer");
+                amount.placeholder = "1";
                 let label = document.createElement("label");
                 label.innerText = "Dauer";
-                label.htmlFor = "form";
+                label.htmlFor = "Dauer";
+                let unit = document.createElement("select");
+                unit.setAttribute("class", "Zeit");
+                var array = ["Stunden", "Minuten"];
+                for (var i = 0; i < array.length; i++) {
+                    var option = document.createElement("option");
+                    option.value = array[i];
+                    option.text = array[i];
+                    unit.appendChild(option);
+                }
+                ;
+                form.appendChild(what);
+                form.appendChild(label);
+                form.appendChild(amount);
+                form.appendChild(unit);
                 break;
             case "Fahrdienst":
-                Fahrdienst; //Warum kann die Funktion nicht ausgeführt werden
+                Fahrdienst; //Warum kann die Funktion nicht ausgeführt werden?
+                let what2 = document.createElement("input");
+                what2.setAttribute("class", "Fahrt");
+                what2.placeholder = "Wohin soll es gehen?";
+                let amount2 = document.createElement("input");
+                amount2.setAttribute("class", "Entfernung");
+                amount2.placeholder = "1";
+                let label2 = document.createElement("label");
+                label2.innerText = "Entfernung";
+                label2.htmlFor = "Entfernung";
+                let unit2 = document.createElement("select");
+                unit2.setAttribute("class", "Distanz");
+                var array = ["km", "minuten", "Stunden"];
+                for (var i = 0; i < array.length; i++) {
+                    var option = document.createElement("option");
+                    option.value = array[i];
+                    option.text = array[i];
+                    unit2.appendChild(option);
+                }
+                ;
+                var array1 = ["Hinfahrt", "Hin-und Rückfahrt"];
+                let radiospan1 = document.createElement("span");
+                for (var i = 0; i < array1.length; i++) {
+                    let where = document.createElement("input");
+                    where.setAttribute("type", "radio");
+                    where.value = array1[i];
+                    where.id = array1[i];
+                    where.name = "Container2";
+                    let label = document.createElement("label");
+                    label.htmlFor = array1[i];
+                    label.innerText = array1[i];
+                    radiospan1.appendChild(label);
+                    radiospan1.appendChild(where);
+                }
+                ;
+                form.appendChild(what2);
+                form.appendChild(label2);
+                form.appendChild(amount2);
+                form.appendChild(unit2);
+                form.appendChild(radiospan1);
                 break;
         }
     }
--- L03/Aufgabe_3/Haushaltshilfe.ts
+++ L03/Aufgabe_3/Haushaltshilfe.ts
@@ -1,11 +1,11 @@
-namespace Haushaltshilfe{
+namespace Haushaltshilfe {
     window.addEventListener("load", handleLoad);
-    
 
-    function handleLoad (_event: Event): void {
-        let Task:HTMLButtonElement = <HTMLButtonElement>document.querySelector("button#Task");
-        let Send:HTMLButtonElement = <HTMLButtonElement>document.querySelector("button#Send");
-        let Help:HTMLElement = <HTMLElement>document.querySelector("#Help");
+
+    function handleLoad(_event: Event): void {
+        let Task: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button#Task");
+        let Send: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button#Send");
+        let Help: HTMLElement = <HTMLElement>document.querySelector("#Help");
 
         Task.addEventListener("click", newTask);
         Send.addEventListener("click", send);
@@ -13,102 +13,198 @@ namespace Haushaltshilfe{
 
 
     }
-    
-    function help (_event: Event): void {
-        alert ("Wie funktioniert es: Um eine neue Erledigung hinzuzufügen klicken Sie auf den Button mit der Beschriftung:neue Erledigung. Um innerhalb einer Erledigung eine Aufgabe/ein Produkt hinzuzufügen bitte auf das Plus klicken. Füllen Sie alle aus Fächer aus. Um ein Produkt/Aufgabe zu löschen auf den kleinen Mülleimer drücken. Man kann auch eine ganze Erledigung löschen, indem man neben dem Plus auf dem Mülleimer drückt. Am Ende nicht vergessen auf Abschicken zu clicken!");
+
+    function help(_event: Event): void {
+        alert("Wie funktioniert es: Um eine neue Erledigung hinzuzufügen klicken Sie auf den Button mit der Beschriftung:neue Erledigung. Um innerhalb einer Erledigung eine Aufgabe/ein Produkt hinzuzufügen bitte auf das Plus klicken. Füllen Sie alle aus Fächer aus. Um ein Produkt/Aufgabe zu löschen auf den kleinen Mülleimer drücken. Man kann auch eine ganze Erledigung löschen, indem man neben dem Plus auf dem Mülleimer drückt. Am Ende nicht vergessen auf Abschicken zu clicken!");
     }
-    function send (_event:Event): void{
-        alert ("Ihr Auftrag wurde abgeschickt");
+    function send(_event: Event): void {
+        alert("Ihr Auftrag wurde abgeschickt");
     }
-    function newTask (_event:Event): void {
-        let Box:HTMLDivElement = document.createElement("div");
-        Box.setAttribute("class","box");
-        let Erledigung:HTMLSelectElement = document.createElement("select");
-        Erledigung.setAttribute("class","Erledigung");
-        var array = ["Einkaufen","Haushalt","Fahrdienst"];
+    function newTask(_event: Event): void {
+        let Box: HTMLDivElement = document.createElement("div");
+        Box.setAttribute("class", "box");
+        let Erledigung: HTMLSelectElement = document.createElement("select");
+        Erledigung.setAttribute("class", "Erledigung");
+        var array = ["Einkaufen", "Haushalt", "Fahrdienst"];
         for (var i = 0; i < array.length; i++) {
             var option = document.createElement("option");
             option.value = array[i];
             option.text = array[i];
-            Erledigung.appendChild(option);};
+            Erledigung.appendChild(option);
+        };
 
-        
-        let plus:HTMLDivElement = document.createElement("div");
+
+        let plus: HTMLDivElement = document.createElement("div");
         plus.setAttribute("class", "addArtikel");
-        plus.innerHTML="+";
-        plus.addEventListener("click",Add);
-        let d_Trash:HTMLElement =<HTMLElement>document.createElement("i");
+        plus.innerHTML = "+";
+        plus.addEventListener("click", Add);
+        let d_Trash: HTMLElement = <HTMLElement>document.createElement("i");
         d_Trash.setAttribute("class", "far fa-trash-alt");
-        
+
         Box.appendChild(Erledigung);
         Box.appendChild(plus);
         Box.appendChild(d_Trash);
-        let column:HTMLFormElement = <HTMLFormElement>document.querySelector("form#column1");
+        let column: HTMLFormElement = <HTMLFormElement>document.querySelector("form#column1");
         column.appendChild(Box);
 
 
 
     }
 
+    //Probleme: Bei ändern der Erledigung müssen die alten forms gelöscht werden
+    //Bei Neuer Erledigung wird bei jedem Case nur noch das Zuletzt genutzte case dargestellt
+    function Add(_event: Event): void {
+        let selectedTask = (<HTMLInputElement>document.querySelector(".Erledigung")).value;
+        console.log(selectedTask);
+        //let currentform:HTMLElement =<HTMLElement>document.querySelector();
+        let deleter: HTMLElement = <HTMLElement>document.createElement("i");
+        deleter.setAttribute("class", "far fa-trash-alt");
+        deleter.setAttribute("id", "trashform");
+        let form: HTMLDivElement = <HTMLDivElement>document.createElement("div");
+        form.setAttribute("class", "form");
+        form.addEventListener("change", displayOrder);
+        let target: Node = <Node>_event.target;
+        let Box: Node = <Node>target.parentNode;
+        Box.appendChild(form);
+        form.appendChild(deleter);
+        switch (selectedTask) {
+            case "Einkaufen":
+                Einkaufen;//Warum kann die Funktion nicht ausgeführt werden?
+                let what1: HTMLInputElement = <HTMLInputElement>document.createElement("input");
+                what1.setAttribute("class", "Artikel");
+                what1.placeholder = "Was möchten Sie Einkaufen?";
+                let amount1: HTMLInputElement = <HTMLInputElement>document.createElement("input");
+                amount1.setAttribute("class", "Menge");
+                amount1.placeholder = "1";
+                let label1: HTMLLabelElement = <HTMLLabelElement>document.createElement("label");
+                label1.innerText = "Menge";
+                label1.htmlFor = "Menge";
+                let unit1: HTMLSelectElement = document.createElement("select");
+                unit1.setAttribute("class", "Einheit");
+                var array = ["Pack", "Gramm", "kg", "Liter","ml","Flaschen", "Leib","Karton"];
+                for (var i = 0; i < array.length; i++) {
+                    var option = document.createElement("option");
+                    option.value = array[i];
+                    option.text = array[i];
+                    unit1.appendChild(option);
+                };
+                var array1 = ["Aldi", "Lidl", "Netto", "Rewe"];
+                let radiospan:HTMLSpanElement= <HTMLSpanElement>document.createElement("span");
+                for (var i = 0; i < array1.length; i++) {
+                    let where:HTMLInputElement = <HTMLInputElement>document.createElement("input");
+                    where.setAttribute("type","radio");
+                    where.value = array1[i];
+                    where.id = array1[i];
+                    where.name="Container";
+                    let label:HTMLLabelElement =<HTMLLabelElement>document.createElement("label");
+                    label.htmlFor=array1[i];
+                    label.innerText=array1[i];
+                    radiospan.appendChild(label);
+                    radiospan.appendChild(where);
+                };
+                form.appendChild(what1);
+                form.appendChild(label1);
+                form.appendChild(amount1);
+                form.appendChild(unit1);
+                form.appendChild(radiospan);
 
-function Add (_event:Event ): void {
-    let selectedTask = (<HTMLInputElement>document.querySelector(".Erledigung")).value;
-    console.log(selectedTask);
-    //let currentform:HTMLElement =<HTMLElement>document.querySelector();
-    let deleter:HTMLElement =<HTMLElement>document.createElement("i");
-    deleter.setAttribute("class", "far fa-trash-alt");
-    let form:HTMLDivElement = <HTMLDivElement>document.createElement("div");
-    form.setAttribute("class", "form");
-    form.addEventListener("change", displayOrder);
-    let Box:HTMLDivElement= <HTMLDivElement>document.querySelector("div.box");
-    Box.appendChild(form);
-    form.appendChild(deleter);
-    switch(selectedTask){
-        case "Einkaufen":
-            Einkaufen;//Warum kann die Funktion nicht ausgeführt werden
-            break;
-        case "Haushalt":
-            Haushalt; //Warum kann die Funktion nicht ausgeführt werden
-            console.log("Haushaltfunction")
-                let what:HTMLInputElement=<HTMLInputElement>document.createElement("input");
-                what.setAttribute("class", "Artikel")
-                what.placeholder="Wie können wir Sie unterstützen?";
-                let amount:HTMLInputElement=<HTMLInputElement>document.createElement("input");
-                amount.setAttribute("class", "Dauer")
-                let label:HTMLLabelElement=<HTMLLabelElement>document.createElement("label");
-                label.innerText="Dauer";
-                label.htmlFor="form";
-                
-            break;
-        case "Fahrdienst":
-            Fahrdienst;//Warum kann die Funktion nicht ausgeführt werden
-            break;
+                break;
+            case "Haushalt":
+                Haushalt; //Warum kann die Funktion nicht ausgeführt werden?
+                console.log("Haushaltfunction")
+                let what: HTMLInputElement = <HTMLInputElement>document.createElement("input");
+                what.setAttribute("class", "Aufgabe");
+                what.placeholder = "Wie können wir Sie unterstützen?";
+                what.setAttribute("list","Aufgaben");//wie Aufgaben Datalist erstellen
+                let amount: HTMLInputElement = <HTMLInputElement>document.createElement("input");
+                amount.setAttribute("class", "Dauer");
+                amount.placeholder = "1";
+                let label: HTMLLabelElement = <HTMLLabelElement>document.createElement("label");
+                label.innerText = "Dauer";
+                label.htmlFor = "Dauer";
+                let unit: HTMLSelectElement = document.createElement("select");
+                unit.setAttribute("class", "Zeit");
+                var array = ["Stunden", "Minuten"];
+                for (var i = 0; i < array.length; i++) {
+                    var option = document.createElement("option");
+                    option.value = array[i];
+                    option.text = array[i];
+                    unit.appendChild(option);
+                };
+
+                form.appendChild(what);
+                form.appendChild(label);
+                form.appendChild(amount);
+                form.appendChild(unit);
+
+                break;
+            case "Fahrdienst":
+                Fahrdienst;//Warum kann die Funktion nicht ausgeführt werden?
+                let what2: HTMLInputElement = <HTMLInputElement>document.createElement("input");
+                what2.setAttribute("class", "Fahrt");
+                what2.placeholder = "Wohin soll es gehen?";
+                let amount2: HTMLInputElement = <HTMLInputElement>document.createElement("input");
+                amount2.setAttribute("class", "Entfernung");
+                amount2.placeholder = "1";
+                let label2: HTMLLabelElement = <HTMLLabelElement>document.createElement("label");
+                label2.innerText = "Entfernung";
+                label2.htmlFor = "Entfernung";
+                let unit2: HTMLSelectElement = document.createElement("select");
+                unit2.setAttribute("class", "Distanz");
+                var array = ["km", "minuten", "Stunden"];
+                for (var i = 0; i < array.length; i++) {
+                    var option = document.createElement("option");
+                    option.value = array[i];
+                    option.text = array[i];
+                    unit2.appendChild(option);
+                };
+                var array1 = ["Hinfahrt", "Hin-und Rückfahrt"];
+                let radiospan1:HTMLSpanElement= <HTMLSpanElement>document.createElement("span");
+                for (var i = 0; i < array1.length; i++) {
+                    let where:HTMLInputElement = <HTMLInputElement>document.createElement("input");
+                    where.setAttribute("type","radio");
+                    where.value = array1[i];
+                    where.id = array1[i];
+                    where.name="Container2";
+                    let label:HTMLLabelElement =<HTMLLabelElement>document.createElement("label");
+                    label.htmlFor=array1[i];
+                    label.innerText=array1[i];
+                    radiospan1.appendChild(label);
+                    radiospan1.appendChild(where);
+                };
+                form.appendChild(what2);
+                form.appendChild(label2);
+                form.appendChild(amount2);
+                form.appendChild(unit2);
+                form.appendChild(radiospan1);
+
+                break;
+        }
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+    function displayOrder(): void {
+        console.log("displayOrder")
     }
-};
-
-
-
-
-
-
-
-
-
-
-
-
-function displayOrder():void{
-    console.log("displayOrder")
-}
-function Einkaufen(): void{
-    console.log("Einkauffunction")
-}
-function Haushalt(): void{
-    console.log("Haushaltfunction")
-}
-function Fahrdienst(): void{
-    console.log("Fahrdienstfunction")
-}
+    function Einkaufen(): void {
+        console.log("Einkauffunction")
+    }
+    function Haushalt(): void {
+        console.log("Haushaltfunction")
+    }
+    function Fahrdienst(): void {
+        console.log("Fahrdienstfunction")
+    }
 
 
 
