c11684a4fd15701ed24a875b780917580c3853f7
Cari1512 <56436935+Cari1512@users.noreply.github.com>
2020-06-06 16:03:18 +0200

    L07 angelegt

---
+++ L07/Client/Data.js
@@ -0,0 +1,37 @@
+"use strict";
+var L06;
+(function (L06) {
+    L06.data = {
+        product: [
+            { name: "Apfel", unit: "kg", price: 10 },
+            { name: "Brot", unit: "Laib", price: 10 },
+            { name: "Milch", unit: "Liter", price: 10 },
+            { name: "Eier", unit: "Karton", price: 10 },
+            { name: "Nudeln", unit: "Gramm", price: 10 },
+            { name: "Zwiebeln", unit: "Stück", price: 10 },
+        ],
+        household: [
+            { name: "Putzen", unit: "1h", price: 10 },
+            { name: "Kochen", unit: "1h", price: 10 },
+            { name: "Gartenarbeit", unit: "1h", price: 10 },
+        ],
+        driving: [
+            { name: "Supermarkt", unit: "km", price: 10 },
+            { name: "Krankenhaus", unit: "km", price: 10 },
+            { name: "Bekannte/Familie", unit: "km", price: 10 },
+        ]
+    };
+    L06.detail = {
+        ort: [
+            { name: "Edeka" },
+            { name: "Aldi" },
+            { name: "Rewe" },
+            { name: "Lidl" }
+        ],
+        fahrt: [
+            { name: "Hin-und Rückfahrt" },
+            { name: "eine Fahrt" },
+        ]
+    };
+})(L06 || (L06 = {}));
+//# sourceMappingURL=Data.js.map
