6a1d786e9a664498a572040454bd0a470ebcc39f
Cari1512 <56436935+Cari1512@users.noreply.github.com>
2021-10-16 21:58:41 +0200

    word strings

--- L01/A01_RandomPoem/poem.ts
+++ L01/A01_RandomPoem/poem.ts
@@ -1,3 +1,7 @@
 namespace poem{
-    
+    let subjects: string[] = ["Summer breezes" , "apple pies" , "sunkissed mangos" , "rain sounds" , "scented candles" ] ;
+    let predicates: string[] = ["brighten", "enhance" , "make better" , "imbrace", "hug"] ;
+    let objects: string[] = ["my day", "lovers", "bad times", "loneliness", "me"];
+
+    console.log(subjects,predicates,objects);
 }
