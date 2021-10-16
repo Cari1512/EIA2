9fb687967c5681e400549836cd49df137efea9b3
Cari1512 <56436935+Cari1512@users.noreply.github.com>
2021-10-16 22:54:08 +0200

    Finishing Poem and in Steckbrief

--- L01/A01_RandomPoem/poem.js
+++ L01/A01_RandomPoem/poem.js
@@ -1,3 +1,22 @@
 "use strict";
-console.log("yay");
+var poem;
+(function (poem) {
+    let subjects = ["Summer breezes", "apple pies", "sunkissed mangos", "rain sounds", "scented candles"];
+    let predicates = ["brighten", "enhance", "intensify", "imbrace", "hug"];
+    let objects = ["my day", "lovers", "bad times", "loneliness", "me"];
+    //console.log(subjects,predicates,objects);
+    for (let index = subjects.length; index > 0; index--) {
+        getVerse(subjects, predicates, objects);
+        //console.log(index);
+    }
+    function getVerse(_subjects, _predicates, _objects) {
+        let vers = " ";
+        let newSubject = Math.floor(Math.random() * _subjects.length);
+        let newPredicate = Math.floor(Math.random() * _predicates.length);
+        let newObject = Math.floor(Math.random() * _objects.length);
+        vers = vers + _subjects.splice(newSubject, 1) + " " + _predicates.splice(newPredicate, 1) + " " + _objects.splice(newObject, 1);
+        console.log(vers);
+        return ("Poem");
+    }
+})(poem || (poem = {}));
 //# sourceMappingURL=poem.js.map
--- L01/A01_RandomPoem/poem.ts
+++ L01/A01_RandomPoem/poem.ts
@@ -1,7 +1,27 @@
 namespace poem{
     let subjects: string[] = ["Summer breezes" , "apple pies" , "sunkissed mangos" , "rain sounds" , "scented candles" ] ;
-    let predicates: string[] = ["brighten", "enhance" , "make better" , "imbrace", "hug"] ;
+    let predicates: string[] = ["brighten", "enhance" , "intensify" , "imbrace", "hug"] ;
     let objects: string[] = ["my day", "lovers", "bad times", "loneliness", "me"];
 
-    console.log(subjects,predicates,objects);
-}
+    //console.log(subjects,predicates,objects);
+
+    for (let index: number = subjects.length; index > 0; index--) {
+        
+        getVerse(subjects, predicates, objects);
+        //console.log(index);
+
+    }
+  
+    
+    function getVerse(_subjects: string[], _predicates: string[], _objects: string[]): string {
+        
+        let vers: string = " ";
+        let newSubject: number = Math.floor(Math.random() * _subjects.length);
+        let newPredicate: number = Math.floor(Math.random() * _predicates.length);
+        let newObject: number = Math.floor(Math.random() * _objects.length);
+        vers = vers + _subjects.splice(newSubject, 1) + " "+ _predicates.splice(newPredicate, 1) + " "+_objects.splice(newObject, 1);
+        console.log(vers);
+        return ("Poem");
+    }
+
+    }
